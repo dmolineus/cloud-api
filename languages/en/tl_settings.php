bad3d4ae39255bc82e9445364a6be03f05ecac71
David Molineus <support@marmalade.de>
2012-11-14 09:29:59 +0100

    change widget label and added missing div close tag

--- languages/en/tl_settings.php
+++ languages/en/tl_settings.php
@@ -1,5 +1,5 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_settings']['cloudapi_legend'] = 'Cloud Api';
-$GLOBALS['TL_LANG']['tl_settings']['cloudapi_accessTokenLink'] = 'Link folgen und Vebdindung erlauben, danach diese Seite neu laden.';
+$GLOBALS['TL_LANG']['tl_settings']['cloudapi_accessTokenLink'] = 'Token anfordern. Danach diese Seite neu laden.';
 $GLOBALS['TL_LANG']['tl_settings']['cloudapi_connected'] = 'verbunden mit:';
