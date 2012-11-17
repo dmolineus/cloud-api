03442f03feba25118eb4803dd46af49ca30b67be
David Molineus <support@marmalade.de>
2012-11-17 17:11:11 +0100

    added comments and remove cache level settings

--- classes/CloudCache.php
+++ classes/CloudCache.php
@@ -1,16 +1,28 @@
 <?php
 
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   cloud-api 
+ * @author    David Molineus <http://www.netzmacht.de>
+ * @license   GNU/LGPL 
+ * @copyright Copyright 2012 David Molineus netzmacht creative 
+ *  
+ **/
+ 
 namespace Netzmacht\Cloud\Api;
 use File;
 use Folder;
 
 /**
- * CloudCache handles caching for the cloud api. It is a static class
+ * CloudCache handles caching for the cloud api. 
+ * Use this class as a static class
  * 
- * @package CloudApi
- * @author David Molineus <mail@netzmacht.de>
- * @copyright Copyright 2012 David Molineus netzmacht creative
- * @license GNU/LGPL
+ * It can be created an object but only for Contao
+ * TL_PURGE purposesIt is a static class
+ * 
  */
 class CloudCache
 {
@@ -32,17 +44,12 @@ class CloudCache
 	/**
 	 * cache content
 	 * 
-	 * @param string $strKey
-	 * @param string $strContent
-	 * @param int cache level (1 for meta file, 2 for file)
+	 * @param string cache key
+	 * @param string kontent to cache
 	 * @return bool
 	 */
-	public static function cache($strKey, $strContent, $intLevel = 2)
+	public static function cache($strKey, $strContent)
 	{
-		if(!self::hasCacheLevel($intLevel)) {
-			return false;
-		}
-		
 		$strFileName = self::getPath($strKey);
 		$objFile = new File($strFileName);
 		$objFile->write($strContent);
@@ -54,8 +61,8 @@ class CloudCache
 	/**
 	 * delete cached content
 	 * 
-	 * @param string $strKey
-	 * @return bool
+	 * @param string cache key
+	 * @return bool true on success
 	 */
 	public static function delete($strKey)
 	{
@@ -63,8 +70,8 @@ class CloudCache
 			return false;			
 		}
 		
-		//$objFile = new File(self::getPath($strKey));
-		//$objFile->delete();
+		$objFile = new File(self::getPath($strKey));
+		$objFile->delete();
 		
 		return true;
 	}
@@ -73,8 +80,8 @@ class CloudCache
 	/**
 	 * get cached content
 	 * 
-	 * @param string $strKey
-	 * @return mixed
+	 * @param string cache key
+	 * @return mixed false if not found or content of cache
 	 */
 	public static function get($strKey)
 	{
@@ -91,7 +98,7 @@ class CloudCache
 	/**
 	 * generate cache file name
 	 * 
-	 * @param string $strKey
+	 * @param string cache key
 	 * @return string file path
 	 */
 	public static function getPath($strKey)
@@ -101,14 +108,6 @@ class CloudCache
 		}
 		
 		return self::$arrFilePaths[$strKey];
-	}
-
-	/**
-	 * check cache level
-	 */
-	public static function hasCacheLevel($intLevel=2)
-	{
-		return ($GLOBALS['cloudApi']['cacheLevel'] >= $intLevel);		
 	}
 	
 			
@@ -123,12 +122,17 @@ class CloudCache
 		return file_exists(TL_ROOT . '/' . self::getPath($strKey));		
 	}
 	
+	
 	/**
 	 * delete all cached files
+	 * this is called by TL_PURGE hook of contao
+	 * 
+	 * @return void
 	 */
 	public function purgeCache()
 	{
 		$objFolder = new Folder(self::CACHE_DIR);
 		$objFolder->purge();		
 	}
+	
 }
