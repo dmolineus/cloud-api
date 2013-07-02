4b1d25a47d7efede6b5b66fb1149605e4b2760fc
David Molineus <support@marmalade.de>
2013-07-02 07:52:01 +0200

    fixed installation issue

--- DataContainer/CloudApi.php
+++ DataContainer/CloudApi.php
@@ -2,16 +2,16 @@
 
 /**
  * Contao Open Source CMS
- * 
+ *
  * Copyright (C) 2005-2012 Leo Feyer
- * 
- * @package   cloud-api 
+ *
+ * @package   cloud-api
  * @author    David Molineus <http://www.netzmacht.de>
- * @license   GNU/LGPL 
- * @copyright Copyright 2012 David Molineus netzmacht creative 
- *  
+ * @license   GNU/LGPL
+ * @copyright Copyright 2012 David Molineus netzmacht creative
+ *
  **/
- 
+
 namespace Netzmacht\Cloud\Api\DataContainer;
 use Netzmacht\Cloud\Api\CloudApiManager;
 use Netzmacht\Utils\DataContainer;
@@ -22,21 +22,21 @@ use Netzmacht\Utils\DataContainer;
  */
 class CloudApi extends DataContainer
 {
-	
+
 	/**
 	 * @var bool
 	 */
 	protected $blnReset = false;
-	
+
 	/**
 	 * @var Netzmacht\Cloud\Api\DataContainer\CloudApi
 	 */
 	protected static $objInstance = null;
-	
-	
+
+
 	/**
 	 * define a singleton because the class is used for datacontainer callbacks and meta palettes callbacks as well
-	 * 
+	 *
 	 * @return Netzmacht\Cloud\Api\DataContainer\CloudApi
 	 */
 	public static function getInstance()
@@ -45,89 +45,87 @@ class CloudApi extends DataContainer
 		{
 			static::$objInstance = new static();
 		}
-		
-		return static::$objInstance;		
+
+		return static::$objInstance;
 	}
-	
-	
+
+
 	/**
 	 * choose palette supports different palettes depending on cloud name
-	 * 
-	 * @return void  
+	 *
+	 * @return void
 	 */
 	public function choosePalette()
 	{
-		try
-		{
-			$objApi = CloudApiManager::getApi(\Input::get('id'));			
-		}
-		catch(\Exception $e)
+		$objApi = $this->Database->prepare('SELECT * FROM tl_cloud_api WHERE id=?')->limit(1)->execute(\Input::get('id'));
+
+		if($objApi->numRows < 1)
 		{
 			return;
 		}
-		
+
+
 		if(isset($GLOBALS['TL_DCA']['tl_cloud_api']['palettes'][$objApi->name]))
 		{
 			$GLOBALS['TL_DCA']['tl_cloud_api']['palettes']['default'] = $GLOBALS['TL_DCA']['tl_cloud_api']['palettes'][$objApi->name];
 		}
 	}
-	
-	
+
+
 	/**
 	 * choose subpaletttes using the palettes__callback because we have to do it before meta palettes renders the palettes
 	 * subpalettes are stores in cloudapi_metasubselectpalettes['cloudname'].
-	 * 
+	 *
 	 * @return void
 	 */
 	public function chooseSubpalettes()
 	{
-		try
-		{
-			$objApi = CloudApiManager::getApi(\Input::get('id'));			
-		}
-		catch(\Exception $e)
+		$objApi = $this->Database->prepare('SELECT * FROM tl_cloud_api WHERE id=?')->limit(1)->execute(\Input::get('id'));
+
+		if($objApi->numRows < 1)
 		{
 			return;
 		}
-		
+
 		if(isset($GLOBALS['TL_DCA']['tl_cloud_api']['cloudapi_metasubselectpalettes'][$objApi->name]))
 		{
 			$GLOBALS['TL_DCA']['tl_cloud_api']['metasubpalettes'] = $GLOBALS['TL_DCA']['tl_cloud_api']['cloudapi_metasubselectpalettes'][$objApi->name];
 		}
 	}
-	
-	
+
+
 	/**
 	 * lets delete all corresponding nodes and mounts
 	 * we did not set up ptable, ctable relations because
-	 * we want to display ALL mounts on one page => dont know any way how to do it with setting ptable 
+	 * we want to display ALL mounts on one page => dont know any way how to do it with setting ptable
 	 * and we use pid in tl_cloud_node as parent node id like tl_files does
-	 * 
+	 *
 	 * @param $objDc
 	 */
 	public function deleteNodesAndMounts($objDc)
 	{
 		$this->import('Database');
-		
+
 		$this->Database->prepare('DELETE FROM tl_cloud_node WHERE cloudapi=?')->execute($objDc->id);
-		$this->Database->prepare('DELETE FROM tl_cloud_mount WHERE pid=?')->execute($objDc->id);		
+		$this->Database->prepare('DELETE FROM tl_cloud_mount WHERE pid=?')->execute($objDc->id);
 	}
-	
-	
+
+
 	/**
 	 * check if mounted folders has changed so we need to reset the sync state
-	 *  
+	 *
 	 */
 	public function resetOnUpdate($mixedValue, $objDc)
 	{
-		$this->blnReset = ($mixedValue != $objDc->activeRecord->mountedFolders);		
+		return $mixedValue;
+		$this->blnReset = ($mixedValue != $objDc->activeRecord->mountedFolders);
 		return $mixedValue;
 	}
-	
-	
+
+
 	/**
 	 * reset sync state if blnReset was set to true
-	 * 
+	 *
 	 * @param $objDc
 	 */
 	public function updateSyncState($objDc)
@@ -137,10 +135,10 @@ class CloudApi extends DataContainer
 		{
 			return;
 		}
-		
+
 		try
 		{
-			$objCloudApi = CloudApiManager::getApi($objDc->id);			
+			$objCloudApi = CloudApiManager::getApi($objDc->id);
 		}
 		catch(\Exception $e)
 		{
@@ -148,7 +146,7 @@ class CloudApi extends DataContainer
 			trigger_error('Could not initiate Cloud API', E_USER_ERROR);
 			return false;
 		}
-		
+
 		$objCloudApi->resetSyncState();
 		return true;
 	}
@@ -156,8 +154,8 @@ class CloudApi extends DataContainer
 
 	/**
 	 * hide reset button if sync state is already reset
-	 * 
-	 * @param string the button name 
+	 *
+	 * @param string the button name
 	 * @param string href
 	 * @param string label
 	 * @param string title
@@ -168,12 +166,12 @@ class CloudApi extends DataContainer
 	{
 		return ($arrRow['deltaCursor'] != null || $arrRow['syncTstamp'] != 0 || $arrRow['syncInProgress'] != '');
 	}
-	
+
 
 	/**
 	 * hide install button if every cloud api is installed
-	 * 
-	 * @param string the button name 
+	 *
+	 * @param string the button name
 	 * @param string href
 	 * @param string label
 	 * @param string title
@@ -183,8 +181,8 @@ class CloudApi extends DataContainer
 	protected function buttonRuleInstallApi(&$strButton, &$strHref, &$strLabel, &$strTitle, &$strIcon, &$strAttributes, &$arrAttributes, $arrRow=null)
 	{
 		$arrApis = CloudApiManager::getApis(0);
-		
+
 		return (count($arrApis) > 0);
 	}
-	
+
 }
--- classes/CloudApi.php
+++ classes/CloudApi.php
@@ -9,180 +9,282 @@
  * @author    David Molineus <http://www.netzmacht.de>
  * @license   GNU/LGPL
  * @copyright Copyright 2012 David Molineus netzmacht creative
- *
  **/
 
-namespace Netzmacht\Cloud\Api\DataContainer;
-use Netzmacht\Cloud\Api\CloudApiManager;
-use Netzmacht\Utils\DataContainer;
-
+namespace Netzmacht\Cloud\Api;
+use System;
 
 /**
- * extends the utils data container
+ * Abstract class for defining the API for CloudApi classes
+ *
  */
-class CloudApi extends DataContainer
+abstract class CloudApi extends System implements syncListenable
 {
 
 	/**
-	 * @var bool
-	 */
-	protected $blnReset = false;
-
-	/**
-	 * @var Netzmacht\Cloud\Api\DataContainer\CloudApi
-	 */
-	protected static $objInstance = null;
-
-
-	/**
-	 * define a singleton because the class is used for datacontainer callbacks and meta palettes callbacks as well
+	 * api config
 	 *
-	 * @return Netzmacht\Cloud\Api\DataContainer\CloudApi
+	 * @var array
 	 */
-	public static function getInstance()
-	{
-		if(static::$objInstance === null)
-		{
-			static::$objInstance = new static();
-		}
+	protected $arrConfig;
 
-		return static::$objInstance;
-	}
+	/**
+	 * store all created nodes
+	 *
+	 * @var array
+	 */
+	protected $arrNodes = array();
 
 
 	/**
-	 * choose palette supports different palettes depending on cloud name
+	 * sync listeners
+	 *
+	 * @var array
+	 */
+	protected $arrSyncListeners = array();
+
+
+	/**
+	 * constructor will load database config
 	 *
 	 * @return void
 	 */
-	public function choosePalette()
+	public function __construct($arrRow)
 	{
-		$objApi = $this->Database->prepare('SELECT * FROM tl_cloud_api WHERE id=?')->limit(1)->execute(\Input::get('id'));
-
-		if($objApi->numRows < 1)
+		if($arrRow === null)
 		{
-			return;
+			throw new \Exception(sprintf('Cloud Service "%s" config is not found. Please install the Cloud service.', $this->name));
 		}
 
+		$this->arrConfig = $arrRow;
 
-		if(isset($GLOBALS['TL_DCA']['tl_cloud_api']['palettes'][$objApi->name]))
-		{
-			$GLOBALS['TL_DCA']['tl_cloud_api']['palettes']['default'] = $GLOBALS['TL_DCA']['tl_cloud_api']['palettes'][$objApi->name];
-		}
-	}
+		// initiate cloud nodes model for limiting statements to current cloud service
+		\CloudNodeModel::setApi($this);
+		CloudNodeModelCollection::setApi($this);
 
-
-	/**
-	 * choose subpaletttes using the palettes__callback because we have to do it before meta palettes renders the palettes
-	 * subpalettes are stores in cloudapi_metasubselectpalettes['cloudname'].
-	 *
-	 * @return void
-	 */
-	public function chooseSubpalettes()
-	{
-		$objApi = $this->Database->prepare('SELECT * FROM tl_cloud_api WHERE id=?')->limit(1)->execute(\Input::get('id'));
-
-		if($objApi->numRows < 1)
-		{
-			return;
-		}
-
-		if(isset($GLOBALS['TL_DCA']['tl_cloud_api']['cloudapi_metasubselectpalettes'][$objApi->name]))
-		{
-			$GLOBALS['TL_DCA']['tl_cloud_api']['metasubpalettes'] = $GLOBALS['TL_DCA']['tl_cloud_api']['cloudapi_metasubselectpalettes'][$objApi->name];
-		}
-	}
-
-
-	/**
-	 * lets delete all corresponding nodes and mounts
-	 * we did not set up ptable, ctable relations because
-	 * we want to display ALL mounts on one page => dont know any way how to do it with setting ptable
-	 * and we use pid in tl_cloud_node as parent node id like tl_files does
-	 *
-	 * @param $objDc
-	 */
-	public function deleteNodesAndMounts($objDc)
-	{
 		$this->import('Database');
-
-		$this->Database->prepare('DELETE FROM tl_cloud_node WHERE cloudapi=?')->execute($objDc->id);
-		$this->Database->prepare('DELETE FROM tl_cloud_mount WHERE pid=?')->execute($objDc->id);
+		$this->loadLanguageFile('cloudapi');
 	}
 
 
 	/**
-	 * check if mounted folders has changed so we need to reset the sync state
+	 * getter implements following keys
 	 *
+	 * @param string name
+	 * 	- int id
+	 *  - string name
+	 * @return mixed
 	 */
-	public function resetOnUpdate($mixedValue, $objDc)
+	public function __get($strKey)
 	{
-		return $mixedValue;
-		$this->blnReset = ($mixedValue != $objDc->activeRecord->mountedFolders);
-		return $mixedValue;
+		switch($strKey)
+		{
+			case 'id':
+			case 'class':
+			case 'enabled':
+			case 'title':
+			{
+				return $this->arrConfig[$strKey];
+				break;
+			}
+
+			case 'modelClass':
+				return 'Netzmacht\Cloud\Api\CloudNodeModel';
+				break;
+
+			case 'name':
+			{
+				return get_class($this);
+				break;
+			}
+
+			default:
+				return parent::__get($strKey);
+		}
 	}
 
 
 	/**
-	 * reset sync state if blnReset was set to true
+	 * authenticate cloud api
 	 *
-	 * @param $objDc
+	 * @throws Exception if no valid token has found
+	 * @return bool
 	 */
-	public function updateSyncState($objDc)
+	abstract public function authenticate();
+
+
+	/**
+	 * call every registered sync listener
+	 *
+	 * @param mixed string or CloudNodeModel current model or path
+	 * @param string action can be info,update,create,delete,error
+	 * @param string provided message
+	 * @param CloudApi passed cloud api object
+	 */
+	public function callSyncListener($strAction, $mixedNodeOrPath=null, $strMessage=null, $objApi=null)
 	{
-		// Return if there is no active record (override all)
-		if (!$objDc->activeRecord || !$this->blnReset)
+		if(empty($this->arrSyncListeners))
 		{
 			return;
 		}
 
-		try
+		//foreach($this->arrSyncListeners as $mixedListener)
+		for($i = 0; $i < count($this->arrSyncListeners); $i++)
 		{
-			$objCloudApi = CloudApiManager::getApi($objDc->id);
+			call_user_func($this->arrSyncListeners[$i], $strAction, $mixedNodeOrPath, $strMessage, $objApi);
 		}
-		catch(\Exception $e)
-		{
-			$this->log('Could not initiate Cloud API for "' . $strTable . '"', 'DataContainer\CloudApi __updateSyncState()', TL_ERROR);
-			trigger_error('Could not initiate Cloud API', E_USER_ERROR);
-			return false;
-		}
-
-		$objCloudApi->resetSyncState();
-		return true;
 	}
 
 
 	/**
-	 * hide reset button if sync state is already reset
+	 * get account info
 	 *
-	 * @param string the button name
-	 * @param string href
-	 * @param string label
-	 * @param string title
-	 * @param string icon class
-	 * @param string added attributes
+	 * @return array
 	 */
-	protected function buttonRuleCanReset(&$strButton, &$strHref, &$strLabel, &$strTitle, &$strIcon, &$strAttributes, &$arrAttributes, $arrRow=null)
+	abstract public function getAccountInfo();
+
+
+	/**
+	 * register a sync listener
+	 *
+	 * @param mixed variable which is callable by call_user_func
+	 * @param string method name
+	 * @param bool true if its a static call
+	 */
+	public function registerSyncListener($mixedSource, $strMethod, $blnCallStatic = false)
 	{
-		return ($arrRow['deltaCursor'] != null || $arrRow['syncTstamp'] != 0 || $arrRow['syncInProgress'] != '');
+		if(is_string($mixedSource) && !$blnCallStatic)
+		{
+			$mixedSource = new $mixedSource();
+		}
+
+		$this->arrSyncListeners[] = array($mixedSource, $strMethod);
 	}
 
 
 	/**
-	 * hide install button if every cloud api is installed
-	 *
-	 * @param string the button name
-	 * @param string href
-	 * @param string label
-	 * @param string title
-	 * @param string icon class
-	 * @param string added attributes
+	 * reset sync state so that the whole cloud service has to sync again
 	 */
-	protected function buttonRuleInstallApi(&$strButton, &$strHref, &$strLabel, &$strTitle, &$strIcon, &$strAttributes, &$arrAttributes, $arrRow=null)
-	{
-		$arrApis = CloudApiManager::getApis(0);
+	abstract public function resetSyncState();
 
-		return (count($arrApis) > 0);
+
+	/**
+	 * Send a file to the browser so the "save as …" dialogue opens
+	 *
+	 * @param Netzmacht\Cloud\Api\CloudNode
+	 */
+	public function sendFileToBrowser($objNode)
+	{
+		// Make sure there are no attempts to hack the file system
+		// Check whether the file exists
+		if ($objNode === null || $objNode->downloadUrl == null)
+		{
+			header('HTTP/1.1 404 Not Found');
+			die('Invalid file name');
+		}
+
+		$arrAllowedTypes = trimsplit(',', strtolower($GLOBALS['TL_CONFIG']['allowedDownload']));
+
+		if (!in_array($objNode->extension, $arrAllowedTypes))
+		{
+			header('HTTP/1.1 403 Forbidden');
+			die(sprintf('File type "%s" is not allowed', $objNode->extension));
+		}
+
+		// Make sure no output buffer is active
+		// @see http://ch2.php.net/manual/en/function.fpassthru.php#74080
+		while (@ob_end_clean());
+
+		// Prevent session locking (see #2804)
+		session_write_close();
+
+		// redirect to download url
+		$this->redirect($objNode->downloadUrl);
+
+		// HOOK: post download callback
+		/* do we need this hook here and how does that affects contao sendFileToBrowser?
+		if (isset($GLOBALS['TL_HOOKS']['postDownload']) && is_array($GLOBALS['TL_HOOKS']['postDownload']))
+		{
+			foreach ($GLOBALS['TL_HOOKS']['postDownload'] as $callback)
+			{
+				static::importStatic($callback[0])->$callback[1]($strFile);
+			}
+		}
+		*/
+
+		// Stop the script
+		exit;
 	}
+
+
+	/**
+	 * update sync state in the cloud api table
+	 *
+	 * @param bool true
+	 * @param string sync cursor
+	 * @param bool true if tstamp should be saved by finishing
+	 * @return void
+	 */
+	public function setSyncState($blnActive, $strCursor=null,  $blnStoreTstamp=true)
+	{
+		$arrParams = array
+		(
+			'tstamp' => time(),
+			'syncInProgress' => ($blnActive) ? '1' : '0',
+		);
+
+		if($strCursor !== null)
+		{
+			$arrParams['deltaCursor'] = $strCursor;
+		}
+
+		if($blnActive === false && $blnStoreTstamp)
+		{
+			$arrParams['syncTstamp'] = time();
+		}
+
+		$objStatement= $this->Database->prepare('UPDATE tl_cloud_api %s WHERE name=?');
+		$objStatement->set($arrParams);
+		$objStatement->execute($this->name);
+
+		$this->callSyncListener($blnActive ? 'start' : 'stop');
+	}
+
+
+	/**
+	 * implements syncing of file system and database
+	 * We use the delta sync method. If the cloud api does not
+	 * support this you have to override this method
+	 *
+	 * @param bool force syncing no matter when last sync was
+	 * @return void
+	 */
+	public function sync($blnForce = false)
+	{
+		// only sync after 10 minutes and make sure that not other clients are also syncing the database
+		if(!$blnForce && (((time() - $this->arrConfig['syncTstamp']) < $GLOBALS['TL_CONFIG']['cloudapiSyncInterval']) || $this->arrConfig['syncInProgress'] == '1'))
+		{
+			return;
+		}
+
+		$arrMounted = null;
+		if($this->arrConfig['mountedFolders'] != '')
+		{
+			$arrMounted = unserialize($this->arrConfig['mountedFolders']);
+		}
+
+		$this->setSyncState(true);
+		$strCursor = $this->execSync($this->arrConfig['deltaCursor'], $arrMounted);
+		$this->setSyncState(false, $strCursor);
+	}
+
+
+	/**
+	 * execute syncing
+	 *
+	 * @param string cursor
+	 * @return string new cursor
+	 */
+	abstract protected function execSync($strCursor, $arrMounted=null);
 
 }
